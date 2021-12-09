15923edf04956b3eaad58e08fbbd94017f64e361
zhangxiang <zhangxiang@yunzhangfang.com>
2021-12-09 14:13:46 +0800

    -

---
+++ packages/rebuild/src/render-router/renderRouter.tsx
@@ -0,0 +1,48 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import React, { ReactNode, useEffect, useState } from "react";
+import { render } from "react-dom";
+
+const renderRoutes: React.FC = (props) => {
+  const {
+    route: { path, component, routes = [], redirect, ...restProps },
+  } = props;
+
+  let Component: ReactNode;
+
+  if (typeof component === "string") {
+    // @ts-ignore
+    Component = React.lazy(() => import(component + ".tsx"));
+  }
+
+  console.log(Component, "comps");
+
+  if (redirect) {
+    return <Redirect key={path} exact {...restProps} from={path} to={redirect} />;
+  }
+
+  if (!redirect && !component && routes.length > 0) {
+    return renderSwitch({ routes });
+  }
+
+  return (
+    <Route
+      key={path}
+      path={path}
+      {...restProps}
+      render={(props) => {
+        return (
+          <React.Suspense fallback={"loading"}>
+            <Component {...props}>
+              {routes.length > 0 && renderSwitch({ routes })}
+            </Component>
+          </React.Suspense>
+        );
+      }}
+    ></Route>
+  );
+};
+
+export const renderSwitch = (props) => {
+  const { routes } = props;
+  return <Switch>{routes.map((route) => renderRoutes({ route }))}</Switch>;
+};
